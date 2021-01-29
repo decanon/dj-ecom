ef71175ab6deacb456ca732568bff9968384770e
hendra <hendradev8@gmail.com>
2021-01-29 19:07:42 +0700

    created app dj_store

---
+++ ecommerce/dj_store/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class DjStoreConfig(AppConfig):
+    name = 'dj_store'
--- ecommerce/ecommerce/settings.py
+++ ecommerce/ecommerce/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'dj_store.apps.DjStoreConfig',
 ]
 
 MIDDLEWARE = [
